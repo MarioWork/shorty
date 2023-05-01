169dd771d5d225f534e820ba95e1d1769dbb45fd
MarioWork <mariovieira.work@hotmail.com>
2023-05-01 18:47:34 +0000

    use express to run multiple cloud functions localy

--- function/index.js
+++ function/index.js
@@ -1,2 +1,14 @@
-exports.retrieve = require('./src/retrieve');
-exports.write = require('./src/write');
+const express = require('express');
+const retrieve = require('./src/retrieve');
+const write = require('./src/write');
+
+//For local development purposes
+const app = express();
+app.use('/retrieve', retrieve);
+app.use('/write', write);
+
+module.exports = {
+    index: app,
+    retrieve,
+    write
+};
