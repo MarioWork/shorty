42773f4c5bcaf539969c8179b63f42a19886c9f8
MarioWork <mariovieira.work@hotmail.com>
2023-05-04 18:22:21 +0000

    Query attempt, missing authorization

--- function/src/retrieve.js
+++ function/src/retrieve.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-    res.send('Retrieve!');
+const { Datastore } = require('@google-cloud/datastore');
+
+const datastore = new Datastore();
+
+module.exports = async (req, res) => {
+    const query = datastore.createQuery('Task');
+
+    const [tasks] = await datastore.runQuery(query);
+    res.send(tasks);
 };
