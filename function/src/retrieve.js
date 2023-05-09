40ef3cc0cc721f7f8f756ce9e06a7dee524abb34
MarioWork <mariovieira.work@hotmail.com>
2023-05-09 18:10:07 +0000

    check if id param is there

--- function/src/retrieve.js
+++ function/src/retrieve.js
@@ -4,14 +4,22 @@ const datastore = new Datastore({
     projectId: process.env.PROJECT_ID
 });
 
-module.exports = async (req, res) => {
-    const id = req.params.id;
+module.exports = async (req, res, next) => {
+    const id = req.baseUrl.split('/')[1];
+
+    if (!id) {
+        res.status(404);
+        return next();
+    }
 
     const query = datastore.createQuery('url').filter('id', '=', id);
 
     const [[value]] = await datastore.runQuery(query);
 
-    if (!value?.url) res.status(404).send();
+    if (!value?.url) {
+        res.status(404).send();
+        return next();
+    }
 
     res.redirect(301, value.url);
 };
