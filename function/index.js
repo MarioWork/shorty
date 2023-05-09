e9f3d56054c7d183b3521dab3de5b4123b1fd288
MarioWork <mariovieira.work@hotmail.com>
2023-05-09 18:09:56 +0000

    new index to export functions

--- function/index.js
+++ function/index.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const retrieve = require('./src/retrieve');
-const write = require('./src/write');
-
-//For local development purposes
-const app = express();
-app.use('/retrieve/:id', retrieve);
-app.use('/write', write);
-
 module.exports = {
-    index: app,
-    retrieve,
-    write
+    retrieve: require('./src/retrieve'),
+    write: require('./src/write')
 };
