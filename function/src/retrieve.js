7f4aca15fbe8218e78ab1a281a09caf2c69a33c7
MarioWork <mariovieira.work@hotmail.com>
2023-05-09 17:29:12 +0000

    change kind to url

--- function/src/retrieve.js
+++ function/src/retrieve.js
@@ -7,11 +7,11 @@ const datastore = new Datastore({
 module.exports = async (req, res) => {
     const id = req.params.id;
 
-    const query = datastore.createQuery('link').filter('id', '=', id);
+    const query = datastore.createQuery('url').filter('id', '=', id);
 
     const [[value]] = await datastore.runQuery(query);
 
-    if (!value?.link) res.status(404).send();
+    if (!value?.url) res.status(404).send();
 
-    res.redirect(301, value.link);
+    res.redirect(301, value.url);
 };
