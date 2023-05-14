35fe4290832dd3b79c243d58affad076aff28134
MarioWork <mariovieira.work@hotmail.com>
2023-05-14 19:55:28 +0000

    configure vuetify

--- frontend/shorty-url/nuxt.config.ts
+++ frontend/shorty-url/nuxt.config.ts
@@ -1,4 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ['@invictus.codes/nuxt-vuetify'],
+  vuetify: {
+    moduleOptions: {
+      treeshaking: true,
+      useIconCDN: true,
+      styles: true,
+      autoImport: true,
+    },
+  },
+});
