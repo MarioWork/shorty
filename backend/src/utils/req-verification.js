13780679df75c82a43fe2adc688d27cc04e3b39f
MarioWork <mariovieira.work@hotmail.com>
2023-05-16 14:51:38 +0000

    verify request host and verify req method

--- backend/src/utils/req-verification.js
+++ backend/src/utils/req-verification.js
@@ -1,4 +1,9 @@
-module.exports = req => {
-    const { api_key } = req.headers;
+module.exports = (req, httpVerbAllowed) => {
+    const { method, headers } = req;
+    const { api_key, host } = headers;
+
+    if (host !== process.env.DEV_DOMAIN && host !== process.env.PROD_DOMAIN) return false;
+    if (method != httpVerbAllowed) return false;
+
     return api_key === process.env.API_KEY;
 };
