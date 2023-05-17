100631669b381c06a09a710ad2b017ee46abc1b5
MarioWork <mariovieira.work@hotmail.com>
2023-05-17 09:37:26 +0000

    remove roles creation and use pre existing roles and add first iteration of function deployment cmd

--- prepare-env.sh
+++ prepare-env.sh
@@ -9,53 +9,32 @@ export PROJECT_ID=$(gcloud config list --format 'value(core.project)') #Curernt 
 export LOCATION=YOUR_LOCATION
 
 ####### SERVICES ########
-gcloud services enable iam.googleapis.com datastore.googleapis.com firestore.googleapis.com
-
-
-####### ROLES ########
-
-#Create a role for write only in the datastore entities
-gcloud iam roles create datastore_entities_write_only \
-    --project=$PROJECT_ID \
-    --title=Datastore entities write only \
-    --description=Datastore Allow to write only datastore entities  \
-    --permissions="datastore.entities.create" \
-    --stage=GA
-
-#Create a role for read only in the datastore entities
-gcloud iam roles create datastore_entities_read_only \
-    --project=$PROJECT_ID \
-    --title=Datastore entities write only \
-    --description=Datastore Allow to read only datastore entities \
-    --permissions="datastore.entities.get" \
-    --stage=GA
-
+gcloud services enable iam.googleapis.com datastore.googleapis.com firestore.googleapis.com cloudfunctions.googleapis.com cloudbuild.googleapis.com
 
 ####### SERVICE ACCOUNTS ########
 
 #Create a service account for Datastore read Cloud Function
-gcloud iam service-accounts create Datastore Read Function \
+gcloud iam service-accounts create datastore-read-function \
     --description="Service account to allow to read only from datastore entities" \
-    --display-name="datastore-read-function"
+    --display-name="Datastore Read Function"
 
-#Assign custom role for reading datastore entities to service account
+#Assign reading datastore entities role to service account
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="serviceAccount:datastore-read-function@$PROJECT_ID.iam.gserviceaccount.com" \
-    --role="projects/$PROJECT_ID/roles/datastore_entities_read_only"
+    --role="roles/datastore.viewer"
 
 #Create a service account for Datastore write Cloud Function
-gcloud iam service-accounts create Datastore Write Function \
-    --description="Service account to allow to read only from datastore entities" \
-    --display-name="datastore-write-function"
+gcloud iam service-accounts create datastore-write-function \
+    --description="Service account to allow to write and read from datastore entities" \
+    --display-name="Datastore Write Function"
 
 #Assign custom role for writing datastore entities to service account
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="serviceAccount:datastore-write-function@$PROJECT_ID.iam.gserviceaccount.com" \
-    --role="projects/$PROJECT_ID/roles/datastore_entities_write_only"
+    --role="roles/datastore.user"
 
 
 ####### DATASTORE ########
-
 gcloud firestore databases create --location=$LOCATION --type=datastore-mode
 
-
+#gcloud functions deploy retrieve-data-first-function --trigger-http --runtime=nodejs18 --entry-point=retrieve --service-account=datastore-read-function@$PROJECT_ID.iam.gserviceaccount.com --region=$LOCATION --allow-unauthenticated --update-env-vars API_KEY=YOUR_API_KEY
