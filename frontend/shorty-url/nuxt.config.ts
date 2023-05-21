268daa3b15616efe38826370575ece9373ecb80f
MarioWork <mariovieira.work@hotmail.com>
2023-05-21 19:25:05 +0000

    configure api key and base api url

--- frontend/shorty-url/nuxt.config.ts
+++ frontend/shorty-url/nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     API_KEY: process.env.API_KEY,
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
   },
   modules: ['@invictus.codes/nuxt-vuetify'],
   vuetify: {
