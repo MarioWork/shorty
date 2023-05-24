e9cf5197bf8af7be28cb16d7c0f43e7a9c164f61
MarioWork <mariovieira.work@hotmail.com>
2023-05-24 19:40:16 +0000

    invoke new function

--- frontend/shorty-url/app.vue
+++ frontend/shorty-url/app.vue
@@ -1,9 +1,17 @@
-<template>{{ data }}</template>
+<template>
+  {{ data }}
+  <v-btn @click.once="onClick">Add Data</v-btn>
+</template>
 
 <script setup>
-  const { get } = await useShortUrl();
+  const { get, create } = await useShortUrl();
 
   const { data, error } = await get('e5841d1c21ba5d6d');
+  console.log('RETRIEVE:');
   console.log(data.value);
   console.log(error.value);
+
+  const onClick = async () => {
+    await create('https://www.facebook.com');
+  };
 </script>
