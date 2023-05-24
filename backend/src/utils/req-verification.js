766663f116573caa7d7c05955870b025dfcd0ff5
MarioWork <mariovieira.work@hotmail.com>
2023-05-24 19:39:46 +0000

    fix spelling

--- backend/src/utils/req-verification.js
+++ backend/src/utils/req-verification.js
@@ -2,7 +2,7 @@ module.exports = ({ req, res, httpVerbAllowed }) => {
     const { method, headers } = req;
     const { api_key } = headers;
 
-    if (method != httpVerbAllowed) res.status(405).send({ message: 'Method not allowed' });
+    if (method != httpVerbAllowed) res.status(405).send({ message: 'HTTP Method not allowed' });
 
     if (api_key !== process.env.API_KEY) res.status(403).send({ message: 'Unauthorized' });
 };
