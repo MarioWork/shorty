d8b19c51613b05fd92b81d4f7aed2db2e89a2883
MarioWork <mariovieira.work@hotmail.com>
2023-05-25 22:01:51 +0000

    make api key public

--- frontend/shorty-url/nuxt.config.ts
+++ frontend/shorty-url/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
-    API_KEY: process.env.API_KEY,
     public: {
+      API_KEY: process.env.API_KEY,
       RETRIEVE_API: process.env.RETRIEVE_API,
       WRITE_API: process.env.WRITE_API,
     },
