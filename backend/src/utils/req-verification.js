22a55764d9a3841cca569c68b4f0e01b60e56bb3
MarioWork <mariovieira.work@hotmail.com>
2023-05-25 21:51:02 +0000

    add options request

--- backend/src/utils/req-verification.js
+++ backend/src/utils/req-verification.js
@@ -1,4 +1,16 @@
+const httpVerbs = require('../enums/http-verbs');
+
 module.exports = ({ req, res, httpVerbAllowed }) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
+    if (req.method === httpVerbs.OPTIONS) {
+        res.set('Access-Control-Allow-Methods', httpVerbAllowed);
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('Access-Control-Max-Age', '3600');
+        res.status(204).send('');
+        return;
+    }
+
     const { method, headers } = req;
     const { api_key } = headers;
 
